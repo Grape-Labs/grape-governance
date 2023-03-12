0d676b56549f1e5972efeb70ff9a83cb2f79c16d
Kirk <kfournia@gmail.com>
2023-03-12 21:55:43 +0200

    minor demo changes

--- src/Admin/GovernanceSnapshot.tsx
+++ src/Admin/GovernanceSnapshot.tsx
@@ -947,7 +947,7 @@ export function GovernanceSnapshotView (this: any, props: any) {
             const strgAccounts = new Array();
             for (var item of response){
                 strgAccounts.push({
-                    label: item.account.identifier,
+                    label: item.account.identifier.replaceAll("Grape","Test"),
                     value: item.publicKey.toBase58()
                 });
             }
