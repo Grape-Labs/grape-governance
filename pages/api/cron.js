38af5c4afe686f60d44acfaed86513d59a05bf9b
Kirk <kfournia@gmail.com>
2023-04-13 11:08:39 +0300

    Cron: minor fixes

--- pages/api/cron.js
+++ pages/api/cron.js
@@ -3,7 +3,7 @@ import cronFetch from '../../src/Admin/Admin';
 
 export default function handler(req, res) {
     // call only if we have a soft wallet
-    cronFetch();
-    
+    //cronFetch();
+
     res.status(200).end('Hello Decentralized Storage Cron!!!');
 }
