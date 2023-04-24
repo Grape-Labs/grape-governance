d79b2e992a7b6773df1622f40471692fb35f9c9e
Kirk <kfournia@gmail.com>
2023-04-24 15:18:50 +0300

    Minor package improvemens

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ import { ApiView } from "./api/api";
 import CssBaseline from '@mui/material/CssBaseline';
 import { inject } from '@vercel/analytics';
 
-import ReactXnft, { AnchorDom, View, Text } from "react-xnft";
+//import ReactXnft, { AnchorDom, View, Text } from "react-xnft";
 
 import {
   Box,
@@ -261,9 +261,9 @@ export const NotFound = () => {
 export default function Dashboard() {
   return <DashboardContent />;
 }
-
+/*
 ReactXnft.render(
   <AnchorDom>
     <DashboardContent />
   </AnchorDom>
-);
+);*/
