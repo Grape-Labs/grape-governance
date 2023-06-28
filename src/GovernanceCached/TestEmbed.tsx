bd5b8ca581dab788073367c04e3844a4c992287d
Kirk <kfournia@gmail.com>
2023-06-28 17:47:59 +0300

    Proposal: Testing embed

--- src/GovernanceCached/TestEmbed.tsx
+++ src/GovernanceCached/TestEmbed.tsx
@@ -3,15 +3,17 @@ import React from 'react';
 function TestEmbed() {
 
     return (
-    <div
-        style={{height: "90%"}}
-    >
-      <iframe 
-        src="https://spl-gov.vercel.app/embedproposal/By2sVGZXwfQq6rAiAM3rNPJ9iQfb5e2QhnF4YjJ4Bip/8yYqWqyDeLFNyLzwyoxvxPvnpYQ9GfSFtMz6aBnNp3eA" 
-        width="500px" 
-        height="500px" 
-        title="Embedded Page"></iframe>
-    </div>
+        <div>
+            <iframe 
+
+                src="https://spl-gov.vercel.app/embedproposal/By2sVGZXwfQq6rAiAM3rNPJ9iQfb5e2QhnF4YjJ4Bip/8yYqWqyDeLFNyLzwyoxvxPvnpYQ9GfSFtMz6aBnNp3eA" 
+                sandbox="allow-same-origin allow-top-navigation allow-scripts allow-forms allow-popup" 
+                width="100%" 
+                height="800" 
+                loading="lazy" 
+                frameBorder="0"
+                title="Proposal Embed Template"></iframe>
+        </div>
   );
 }
 
