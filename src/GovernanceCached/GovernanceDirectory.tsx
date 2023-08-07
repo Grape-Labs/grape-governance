95b72496a766636cc90d7bd70db42230e6930177
Kirk <kfournia@gmail.com>
2023-08-07 14:01:00 +0300

    Directory: Treasury SOL value added p/governance

--- src/GovernanceCached/GovernanceDirectory.tsx
+++ src/GovernanceCached/GovernanceDirectory.tsx
@@ -182,7 +182,7 @@ function GovernanceCardView(props:any) {
                     }
 
                     {item?.totalVaultSolValue?
-                        <>{item.totalVaultStableCoinValue > 1 ?
+                        <>{item.totalVaultSolValue > 1 ?
                         <Typography variant="body2">
                             <>Treasury in Solana <strong>{getFormattedNumberToLocale(Number(item.totalVaultSolValue).toFixed(2))} SOL</strong></>
                         </Typography>
