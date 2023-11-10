9b11a6d64a16b54e932240c72e4669c218ca23e5
Kirk <kfournia@gmail.com>
2023-11-10 10:16:31 +0200

    Minor all proposals fix

--- src/GovernanceCached/GovernanceRealtime.tsx
+++ src/GovernanceCached/GovernanceRealtime.tsx
@@ -450,7 +450,7 @@ function TablePaginationActions(props) {
                                 <TableRow>
                                     <TablePagination
                                     rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
-                                    colSpan={6}
+                                    colSpan={7}
                                     count={proposals && proposals.length}
                                     rowsPerPage={rowsPerPage}
                                     page={page}
