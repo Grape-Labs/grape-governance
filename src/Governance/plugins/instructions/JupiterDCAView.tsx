3b8723741379b974364dc2aa68f516bb545c7e69
Kirk <kfournia@gmail.com>
2024-02-01 20:14:07 +0200

    added strict token list for DCA

--- src/Governance/plugins/instructions/JupiterDCAView.tsx
+++ src/Governance/plugins/instructions/JupiterDCAView.tsx
@@ -140,41 +140,49 @@ export default function JupiterDCAView(props: any) {
         {
             mint:"So11111111111111111111111111111111111111112",
             name:"SOL",
+            symbol:"SOL",
             decimals:9,
             logo:"https://cdn.jsdelivr.net/gh/saber-hq/spl-token-icons@master/icons/101/So11111111111111111111111111111111111111112.png"
         },{
             mint:"EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v",
             name:"USDC",
+            symbol:"USDC",
             decimals:6,
             logo:"https://cdn.jsdelivr.net/gh/saber-hq/spl-token-icons@master/icons/101/EPjFWdd5AufqSSqeM2qN1xzybapC8G4wEGGkZwyTDt1v.png"
         },{
             mint:"Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB",
             name:"USDT",
+            symbol:"USDT",
             decimals:6,
             logo:"https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/Es9vMFrzaCERmJfrF4H2FYD4KCoNkY11McCe8BenwNYB/logo.svg"
         },{
             mint:"mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So",
             name:"mSol",
+            symbol:"mSol",
             decimals:9,
             logo:"https://raw.githubusercontent.com/solana-labs/token-list/main/assets/mainnet/mSoLzYCxHdYgdzU16g5QSh3i5K3z3KZK7ytfqcJm7So/logo.png"
         },{
             mint:"8upjSpvjcdpuzhfR1zriwg5NXkwDruejqNE9WNbPRtyA",
             name:"GRAPE",
+            symbol:"GRAPE",
             decimals:6,
             logo:"https://lh3.googleusercontent.com/y7Wsemw9UVBc9dtjtRfVilnS1cgpDt356PPAjne5NvMXIwWz9_x7WKMPH99teyv8vXDmpZinsJdgiFQ16_OAda1dNcsUxlpw9DyMkUk=s0"
         },{
             mint:"AZsHEMXd36Bj1EMNXhowJajpUXzrKcK57wW4ZGXVa7yR",
             name:"GUAC",
+            symbol:"GUAC",
             decimals:5,
             logo:"https://shdw-drive.genesysgo.net/36JhGq9Aa1hBK6aDYM4NyFjR5Waiu9oHrb44j1j8edUt/image.png"
         },{
             mint:"BaoawH9p2J8yUK9r5YXQs3hQwmUJgscACjmTkh8rMwYL",
             name:"ALL",
+            symbol:"ALL",
             decimals:6,
             logo:"https://arweave.net/FY7yQGrLCAvKAup_SYEsHDoTRZXsttuYyQjvHTnOrYk"
         },{
             mint:"DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263",
             name:"BONK",
+            symbol:"BONK",
             decimals:5,
             logo:"https://arweave.net/hQiPZOsRZXGXBJd_82PhVdlM_hACsT_q6wqwf5cSY7I"
         }]);
@@ -673,7 +681,7 @@ export default function JupiterDCAView(props: any) {
                             alignItems="center"
                         >
                             <Grid item>
-                                <Avatar alt={item.name} src={item.logo} />
+                                <Avatar alt={item.symbol} src={item.logo} />
                             </Grid>
                             <Grid item xs sx={{ml:1}}>
                                 <Grid container
@@ -681,7 +689,7 @@ export default function JupiterDCAView(props: any) {
                                     alignItems="center">
                                         <Grid item>
                                             <Typography variant="h6">
-                                                {item.name}
+                                                {item.symbol}
                                             </Typography>
                                         </Grid>
                                         <Grid item xs sx={{textAlign:'right'}}>
@@ -785,7 +793,8 @@ export default function JupiterDCAView(props: any) {
                         // fix to push only what we have not already added
                         availableTokens.push({
                             mint:item.address,
-                            name:item.name,//item.symbol,
+                            name:item.name,
+                            symbol:item.symbol,
                             decimals:item.decimals,
                             logo:item.logoURI
                         
