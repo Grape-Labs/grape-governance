7276ef8191e0273dba6e971a08116be72e89f83a
Kirk <kfournia@gmail.com>
2024-02-17 17:44:36 +0200

    added devnet support

--- src/utils/grapeTools/constants.ts
+++ src/utils/grapeTools/constants.ts
@@ -9,21 +9,29 @@ export const APP_LOGO = STATIC_LOGO;
 export const CREATOR_LOGO = STATIC_CREATOR_LOGO;
 export const TX_RPC_ENDPOINT = process.env.REACT_APP_API_TX_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
 export const QUICKNODE_RPC_ENDPOINT = process.env.REACT_APP_API_QUICKNODE_RPC_ENDPOINT;
-export const HELIUS_RPC_ENDPOINT = 'https://rpc.helius.xyz/?api-key='+process.env.REACT_APP_API_HELIUS;
+export const QUICKNODE_RPC_DEVNET_ENDPOINT = process.env.REACT_APP_API_QUICKNODE_RPC_DEVNET_ENDPOINT;
+export const HELIUS_RPC_ENDPOINT = 'https://mainnet.helius-rpc.com/?api-key='+process.env.REACT_APP_API_HELIUS;
+export const HELIUS_RPC_DEVNET_ENDPOINT = 'https://devnet.helius.xyz/?api-key='+process.env.REACT_APP_API_HELIUS;
 export const ALCHEMY_RPC_ENDPOINT = process.env.REACT_APP_API_ALCHEMY_RPC_ENDPOINT;
+export const ALCHEMY_RPC_DEVNET_ENDPOINT = process.env.REACT_APP_API_ALCHEMY_RPC_DEVNET_ENDPOINT;
 export const HELLO_MOON_BEARER = process.env.REACT_APP_API_HELLOMOON_API_KEY;
 export const HELLO_MOON_ENDPOINT = HELLO_MOON_BEARER ? `https://rpc.hellomoon.io/${HELLO_MOON_BEARER}` : null;
+export const HELLO_MOON_DEVNET_ENDPOINT = HELLO_MOON_BEARER ? `https://rpc-devnet.hellomoon.io/${HELLO_MOON_BEARER}` : null;
 export const SHYFT_KEY = process.env.REACT_APP_API_SHYFT_KEY;
 export const SHYFT_RPC_ENDPOINT = SHYFT_KEY ? `https://rpc.shyft.to?api_key=${SHYFT_KEY}` : null;
+export const SHYFT_RPC_DEVNET_ENDPOINT = SHYFT_KEY ? `https://devnet-rpc.shyft.to?api_key=${SHYFT_KEY}` : null;
 export const RPC_ENDPOINT = ALCHEMY_RPC_ENDPOINT || QUICKNODE_RPC_ENDPOINT || ALCHEMY_RPC_ENDPOINT || SHYFT_RPC_ENDPOINT || HELLO_MOON_ENDPOINT || HELIUS_RPC_ENDPOINT || 'https://api.mainnet-beta.solana.com';
+export const RPC_DEVNET_ENDPOINT = ALCHEMY_RPC_DEVNET_ENDPOINT || QUICKNODE_RPC_DEVNET_ENDPOINT || SHYFT_RPC_DEVNET_ENDPOINT || HELLO_MOON_DEVNET_ENDPOINT || HELIUS_RPC_DEVNET_ENDPOINT || 'https://api.devnet.solana.com';
 export const WS_ENDPOINT = process.env?.REACT_APP_API_QUICKNODE_RPC_ENDPOINT ? process.env.REACT_APP_API_QUICKNODE_RPC_ENDPOINT.replace('https://', 'wss://') : 'wss://api.mainnet-beta.solana.com';
-
 export const ALCHEMY_ETH_KEY = process.env.REACT_APP_API_ALCHEMY_ETH || null;
 export const WALLET_CONNECT_PROJECT_ID = process.env.REACT_APP_API_WALLET_CONNECT_PROJECT_ID || null;
 export const DYNAMICXYZ_KEY = process.env.REACT_APP_API_DYNAMICXYZ_KEY || null;
 
 export const RPC_CONNECTION = new Connection(
     RPC_ENDPOINT, 'confirmed'
+);
+export const RPC_DEVNET_CONNECTION = new Connection(
+    RPC_DEVNET_ENDPOINT, 'confirmed'
 );
 
 export const MAILGUN_KEY = process.env.REACT_APP_API_MAILGUN_KEY || null;
