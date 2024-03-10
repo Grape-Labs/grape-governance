28ee0218a2b5b50ec23d48b7121c8b3f25176701
Kirk <kfournia@gmail.com>
2024-03-10 13:55:06 +0200

    cleaned up voting plugin

--- src/utils/governanceTools/components/instructions/createVote.ts
+++ src/utils/governanceTools/components/instructions/createVote.ts
@@ -47,7 +47,7 @@ import {
 } from "./account";
 // end plugin stuff
 
-//import { VotingClient } from '@utils/uiTypes/VotePlugin'
+import { getVotingPlugin } from './getVotePlugin';
 
 const connection = RPC_CONNECTION;
 
@@ -340,87 +340,4 @@ export const createCastVoteTransaction = async (
         return null;
     }
 
-  };
-    
-  const getVotingPlugin = async (
-    selectedRealm: any,
-    communityMint: any,
-    walletPubkey: any,
-    voterWeightAddin:any,
-  ) => {
-    const options = AnchorProvider.defaultOptions();//AnchorProvider.defaultOptions();
-    
-    const provider = new AnchorProvider(
-      connection,
-      walletPubkey,
-      options
-    );
-    
-    
-    //const pluginPk =
-    //  votingPop === 'community'
-    //    ? realmConfig?.account.communityTokenConfig.voterWeightAddin
-    //    : realmConfig?.account.councilTokenConfig.voterWeightAddin
-    
-
-    //const client = await VsrClient.connect(provider, false);
-
-    const client = await VsrClient.connect(provider, voterWeightAddin, false);
-    const clientProgramId = client!.program.programId;
-
-    //const vwa = selectedRealm?.account?.communityTokenConfig?.voterWeightAddin
-    //const vwa2 = selectedRealm?.account?.councilTokenConfig?.voterWeightAddin
-    
-    //console.log("vwa "+vwa?.toBase58())
-    //console.log("vwa2 "+vwa2?.toBase58())
-
-    console.log("clientProgramId "+clientProgramId.toBase58())
-    
-    console.log("realm: "+new PublicKey(selectedRealm.pubkey).toBase58());
-    console.log("mint: "+communityMint.toBase58());
-    
-    const { registrar } = await getRegistrarPDA(
-      new PublicKey(selectedRealm!.pubkey),
-      new PublicKey(communityMint),
-      clientProgramId
-    );
-    //const registrar = new PublicKey("4WQSYg21RrJNYhF4251XFpoy1uYbMHcMfZNLMXA3x5Mp");
-    console.log("registrar: "+registrar.toBase58());
-    const { voter } = await getVoterPDA(registrar, walletPubkey, clientProgramId);
-    console.log("voter: "+voter.toBase58());
-
-    //const { voterWeightPk } = await getVoterWeightRecord(
-    //  new PublicKey(selectedRealm!.pubkey),
-    //  communityMint,
-    //  walletPubkey,
-    //  clientProgramId
-    //);
-    
-    const { voterWeightPk } = await getVoterWeightPDA(
-      registrar,
-      walletPubkey,
-      clientProgramId,
-    );
-    
-    const { maxVoterWeightRecord } = await getMaxVoterWeightRecord(
-      registrar,
-      communityMint,
-      clientProgramId
-    );
-
-    console.log(walletPubkey.toBase58()+" voterWeightPk: "+voterWeightPk.toBase58());
-    console.log(walletPubkey.toBase58()+" maxVoterWeightRecord: "+maxVoterWeightRecord?.toBase58());
-    
-    /*
-    const updateVoterWeightRecordIx = await client!.program.methods
-      .updateVoterWeightRecord()
-      .accounts({
-        registrar,
-        voter,
-        voterWeightRecord: voterWeightPk,
-        systemProgram: 'GovER5Lthms3bLBqWub97yVrMmEogzX7xNjdXpPPCVZw',
-      })
-      .instruction();
-    */
-    return { voterWeightPk, maxVoterWeightRecord, client, registrar, voter };
-};
+}
