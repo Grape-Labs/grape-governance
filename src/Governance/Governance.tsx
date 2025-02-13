83b4c5b8319ea561b7cc79a201a6f124412b2b72
Kirk <kfournia@gmail.com>
2025-02-13 19:19:35 +0200

    added headerview for quickly adding support for GSPL on various pages in each governance

--- src/Governance/Governance.tsx
+++ src/Governance/Governance.tsx
@@ -52,6 +52,7 @@ import {
 } from './CachedStorageHelpers'; 
 import { createCastVoteTransaction } from '../utils/governanceTools/components/instructions/createVote';
 import { GovernanceProposalDialog } from './GovernanceProposalDialog';
+import { GovernanceHeaderView } from './GovernanceHeaderView';
 import moment from 'moment';
 
 import VerifiedIcon from '@mui/icons-material/Verified';
@@ -1767,110 +1768,12 @@ export function GovernanceCachedView(props: any) {
                                         }}
                                     >
                                         
-                                        <Helmet>
-                                            <meta name="msapplication-TileImage" content="./public/ms-icon-144x144.png"/>
-                                            <meta name="msapplication-TileColor" content="#180A1E"/>
-                                            <meta name="msapplication-TileImage" content="./public/ms-icon-144x144.png"/>
-                                        
-                                            <meta name="description" content={`${realmName} powered by Governance.so by Grape`} />
-                                            <title>{`${realmName}`}</title>
-                                            
-                                            <meta property="og:url" content="https://governance.so"/>
-                                            <meta property="og:type" content="website"/>
-                                            <meta property="og:title" content={`${realmName}`}/>
-                                            <meta property="og:description" content={`${realmName} powered by Governance.so by Grape`}/>
-                                            <meta property="og:image" content="https://shdw-drive.genesysgo.net/5nwi4maAZ3v3EwTJtcg9oFfenQUX7pb9ry4KuhyUSawK/governancesocialsplash.png"/>  
-                                            
-                                            <meta name="twitter:card" content="summary_large_image"/>
-                                            <meta name="twitter:title" content={`${realmName}`}/>
-                                            <meta name="twitter:site" content="@grapeprotocol"/>
-                                            <meta name="twitter:description" content={`${realmName} powered by Governance.so by Grape`}/>
-                                            <meta name="twitter:image" content="https://shdw-drive.genesysgo.net/5nwi4maAZ3v3EwTJtcg9oFfenQUX7pb9ry4KuhyUSawK/governancesocialsplash.png"/>
-                                            <meta name="twitter:image:alt" content={`${realmName}`}/>
-                                        </Helmet>
-                                        
-                                        <Grid item xs={6} container justifyContent="flex-start">
-                                            <Grid container>
-                                                <Grid item xs={12}>
-                                                    
-                                                    {gsplMetadata ?
-                                                        <Grid container alignItems="center" spacing={1}>
-                                                            {/* Governance Image as a Small Circular Icon */}
-                                                            {gsplMetadata?.metadata?.ogImage && !gsplMetadata.metadata.ogImage.endsWith("/") && (
-                                                                <Grid item>
-                                                                    <Avatar 
-                                                                        src={gsplMetadata.metadata.ogImage.startsWith("http") ? gsplMetadata.metadata.ogImage : `https://realms.today${gsplMetadata.metadata.ogImage}`} 
-                                                                        alt={gsplMetadata.metadata.displayName || realmName}
-                                                                        sx={{
-                                                                            width: 40, // Small and consistent
-                                                                            height: 40, // Small and consistent
-                                                                            boxShadow: "0px 4px 10px rgba(0,0,0,0.3)" // Subtle shadow effect
-                                                                        }}
-                                                                    />
-                                                                </Grid>
-                                                            )}
+                                        <GovernanceHeaderView
+                                            governanceName={realmName}
+                                            governanceAddress={governanceAddress}
+                                            gsplMetadata={gsplMetadata}
+                                        />
 
-                                                            {/* Governance Name & Verified Badge */}
-                                                            <Grid item sx={{ display: "flex", alignItems: "center" }}>
-                                                                
-                                                                <Typography variant="h5">
-                                                                    {gsplMetadata?.metadata?.displayName || realmName}
-                                                                </Typography>
-                                                            
-                                                            
-                                                                {/* Verified Checkmark (if item is verified) */}
-                                                                
-                                                                    <Tooltip title="Verified Governance">
-                                                                        <VerifiedIcon 
-                                                                            sx={{ 
-                                                                                fontSize: 20, 
-                                                                                color: "#4CAF50", 
-                                                                                marginLeft: 1, 
-                                                                                opacity: 0.8, 
-                                                                                boxShadow: "0px 2px 5px rgba(0,0,0,0.2)" 
-                                                                            }} 
-                                                                        />
-                                                                    </Tooltip>
-                                                                
-                                                            </Grid>
-                                                        </Grid>
-                                                        
-                                                    :
-                                                        <Typography variant="h4">
-                                                            {realmName}
-                                                        </Typography>
-                                                    }
-                                                </Grid>
-                                                <Grid item xs={12}>    
-                                                    <ButtonGroup>
-                                                        <Tooltip title={`Share ${realmName ? realmName : ''} Governance`}>
-                                                            <Button
-                                                                aria-label="share"
-                                                                variant="outlined"
-                                                                color="inherit"
-                                                                onClick={() => {
-                                                                    if (navigator.share) {
-                                                                        navigator.share({
-                                                                            title: `${realmName} Governance`,
-                                                                            text: `Visit the ${realmName} DAO:`,
-                                                                            url: `https://governance.so/dao/${governanceAddress}`
-                                                                        }).catch((error) => console.error('Error sharing:', error));
-                                                                    } else {
-                                                                        alert("Your browser doesn't support the Share API.");
-                                                                    }
-                                                                }}
-                                                                sx={{
-                                                                    borderRadius:'17px',
-                                                                    borderColor:'rgba(255,255,255,0.05)',
-                                                                    fontSize:'10px'}}
-                                                            >
-                                                                <ShareIcon fontSize='inherit' sx={{mr:1}} /> Share
-                                                            </Button>
-                                                        </Tooltip>
-                                                    </ButtonGroup>
-                                                </Grid>
-                                            </Grid>
-                                        </Grid>
                                         {(showGovernanceNavigation && realm) ?
                                             <Grid item xs={6} container justifyContent="flex-end">
                                                 <GovernanceNavigation governanceAddress={governanceAddress} cachedMemberMap={cachedMemberMap} realm={realm} />
