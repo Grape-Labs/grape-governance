d35b299fa613745b561f20afc6e64260a86c3895
Kirk <kfournia@gmail.com>
2025-08-26 15:05:29 +0300

    added directory image override

--- src/Governance/GovernanceDirectoryCardView.tsx
+++ src/Governance/GovernanceDirectoryCardView.tsx
@@ -2,6 +2,7 @@ import { Card, CardContent, CardActions, Grid, Typography, Tooltip, Button, Icon
 import { Link } from "react-router-dom";
 import VerifiedIcon from "@mui/icons-material/Verified";
 import moment from "moment";
+import { GRAPE_LOGO } from '../utils/grapeTools/constants';
 
 function GovernanceDirectoryCardView(props: any) {
     const { item, metadata } = props;
@@ -22,8 +23,14 @@ function GovernanceDirectoryCardView(props: any) {
                     {metadata?.ogImage && !metadata.ogImage.endsWith("/") && (
                         <Grid item>
                             <Avatar 
-                                src={metadata.ogImage.startsWith("http") ? metadata.ogImage : `https://realms.today${metadata.ogImage}`} 
-                                alt={metadata.displayName || item.governanceName}
+                                src={
+                                    metadata?.ogImage === "/realms/Grape/img/grape.png"
+                                        ? GRAPE_LOGO
+                                        : (metadata?.ogImage?.startsWith("http")
+                                            ? metadata.ogImage
+                                            : `https://realms.today${metadata?.ogImage}`)
+                                }
+                                alt={metadata?.displayName || item.governanceName}
                                 sx={{
                                     width: 40, // Small and consistent
                                     height: 40, // Small and consistent
